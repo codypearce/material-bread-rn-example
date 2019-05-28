6fac026ee7cf5902e4266edbe26fa63563986cfc
codypearce <codyapearce@gmail.com>
2019-05-28 05:38:32 -0700

    bump version v0.1.5

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Root from "./Root";
+import { YellowBox } from "react-native";
 import { BreadProvider } from "material-bread";
 
 export default class App extends Component {
   render() {
+    YellowBox.ignoreWarnings(["Require cycle:"]);
     return (
       <BreadProvider>
         <Root />
--- Root.js
+++ Root.js
@@ -1,53 +1,224 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { Fab, Icon, Appbar, Badge, IconButton } from "material-bread";
 
-export default class App extends React.Component {
+import {
+  Backdrop,
+  List,
+  ListItem,
+  Icon,
+  IconButton,
+  Avatar,
+  Heading
+} from "material-bread";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    alignItems: "center"
+  },
+  backdropHeader: {
+    height: 56,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  backdropHeaderTitle: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "500",
+    marginLeft: 72
+  }
+});
+
+const backLayerRevealed = (
+  <View style={{ flex: 1, width: "100%" }}>
+    <View style={styles.backdropHeader}>
+      <Text style={styles.backdropHeaderTitle}>Navigation</Text>
+    </View>
+
+    <List
+      style={{
+        width: "100%",
+        backgroundColor: "transparent"
+      }}
+    >
+      <ListItem
+        text={"Home"}
+        textStyle={{ color: "white" }}
+        selected
+        style={{ backgroundColor: "transparent", flex: 1 }}
+        icon={<Icon name={"home"} size={24} color={"white"} />}
+      />
+      <ListItem
+        text={"Music"}
+        textStyle={{ color: "white" }}
+        style={{ backgroundColor: "transparent" }}
+        icon={<Icon name={"music-note"} size={24} color={"white"} />}
+      />
+      <ListItem
+        text={"Favorites"}
+        textStyle={{ color: "white" }}
+        style={{ backgroundColor: "transparent" }}
+        icon={<Icon name={"favorite"} size={24} color={"white"} />}
+      />
+      <ListItem
+        text={"Settings"}
+        textStyle={{ color: "white" }}
+        style={{ backgroundColor: "transparent" }}
+        icon={<Icon name={"settings"} size={24} color={"white"} />}
+      />
+    </List>
+  </View>
+);
+
+const album = (
+  <Avatar
+    type="icon"
+    content="album"
+    contentColor={"#ececec"}
+    color={"#a3a3a3"}
+    size={40}
+  />
+);
+const iconFav = <IconButton name="favorite" size={24} color="#6e6e6e" />;
+
+export default class Page extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Appbar
-          barType={"normal"}
-          title={"Page Title"}
-          navigationType={"menu"}
-          onNavigation={() => console.log("onNavigation!")}
-          actionItems={[
-            <Badge
-              containerStyle={{ marginRight: 16, flex: 1 }}
-              color={"#e10050"}
-              textColor={"white"}
-              size={14}
-              content={77}
-            >
-              <IconButton name="favorite" size={24} color={"white"} />
-            </Badge>,
-            { name: "search", onPress: () => console.log("onSearch") },
-            { name: "more-vert" }
-          ]}
-        />
-        <View style={styles.content}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Fab style={styles.fab} />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <View
+          style={{
+            flex: 1,
+            width: "100%"
+          }}
+        >
+          <Backdrop
+            backLayerConcealed={
+              <View style={styles.backdropHeader}>
+                <Text style={styles.backdropHeaderTitle}>App</Text>
+              </View>
+            }
+            backLayerRevealed={
+              <View style={{ flex: 1, width: "100%" }}>
+                <View style={styles.backdropHeader}>
+                  <Text style={styles.backdropHeaderTitle}>Navigation</Text>
+                </View>
+
+                <List
+                  style={{
+                    width: "100%",
+                    backgroundColor: "transparent"
+                  }}
+                >
+                  <ListItem
+                    text={"Home"}
+                    textStyle={{ color: "white" }}
+                    selected
+                    style={{ backgroundColor: "transparent" }}
+                    icon={<Icon name={"home"} size={24} color={"white"} />}
+                  />
+                  <ListItem
+                    text={"Music"}
+                    textStyle={{ color: "white" }}
+                    style={{ backgroundColor: "transparent" }}
+                    icon={
+                      <Icon name={"music-note"} size={24} color={"white"} />
+                    }
+                  />
+                  <ListItem
+                    text={"Favorites"}
+                    textStyle={{ color: "white" }}
+                    style={{ backgroundColor: "transparent" }}
+                    icon={<Icon name={"favorite"} size={24} color={"white"} />}
+                  />
+                  <ListItem
+                    text={"Settings"}
+                    textStyle={{ color: "white" }}
+                    style={{ backgroundColor: "transparent" }}
+                    icon={<Icon name={"settings"} size={24} color={"white"} />}
+                  />
+                </List>
+              </View>
+            }
+            offset={260}
+          >
+            <View style={styles.container}>
+              <Heading
+                text={"Albums"}
+                style={{
+                  alignSelf: "flex-start",
+                  marginLeft: 20,
+                  fontSize: 20
+                }}
+              />
+              <ListItem
+                text={"Back in Black"}
+                secondaryText={"AC/DC"}
+                media={
+                  <Avatar
+                    type="icon"
+                    content="album"
+                    contentColor={"#ececec"}
+                    color={"#a3a3a3"}
+                    size={40}
+                  />
+                }
+                actionItem={
+                  <IconButton name="favorite" size={24} color="#6e6e6e" />
+                }
+              />
+              <ListItem
+                text={"Hotel California"}
+                secondaryText={"Eagles"}
+                media={
+                  <Avatar
+                    type="icon"
+                    content="album"
+                    contentColor={"#ececec"}
+                    color={"#a3a3a3"}
+                    size={40}
+                  />
+                }
+                actionItem={
+                  <IconButton name="favorite" size={24} color="#6e6e6e" />
+                }
+              />
+              <ListItem
+                text={"Dark Side of the Moon"}
+                secondaryText={"Pink Floyd "}
+                media={
+                  <Avatar
+                    type="icon"
+                    content="album"
+                    contentColor={"#ececec"}
+                    color={"#a3a3a3"}
+                    size={40}
+                  />
+                }
+                actionItem={
+                  <IconButton name="favorite" size={24} color="#6e6e6e" />
+                }
+              />
+              <ListItem
+                text={"Led Zeppelin IV"}
+                secondaryText={"Led Zeppelin"}
+                media={
+                  <Avatar
+                    type="icon"
+                    content="album"
+                    contentColor={"#ececec"}
+                    color={"#a3a3a3"}
+                    size={40}
+                  />
+                }
+                actionItem={
+                  <IconButton name="favorite" size={24} color="#6e6e6e" />
+                }
+              />
+            </View>
+          </Backdrop>
         </View>
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1
-  },
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    position: "relative"
-  },
-  fab: {
-    position: "absolute",
-    right: 24,
-    bottom: 24
-  }
-});
